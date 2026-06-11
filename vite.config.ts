ed1bdb30bc456d2851b01d2d83e19ac3b41f0114
BAS997N <avielias0@gmail.com>
2026-06-12 00:23:08 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
 
-export default defineConfig(() => {
+export default defineConfig({
   base: '/Status/',
-    plugins: [react(), tailwindcss()],
+  plugins: [react()],
+})
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
