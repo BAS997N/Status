76dc83b87e900073e1f6a3b6551a98a8706ba902
BAS997N <avielias0@gmail.com>
2026-06-12 01:08:05 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-export default defineConfig({
-  base: '/Status/',
-  plugins: [react()],
+export default defineConfig(() => {
+  return {
+    base: '/Status/',
 
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
+    plugins: [react(), tailwindcss()],
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
     },
-  },
 
-  server: {
-    hmr: process.env.DISABLE_HMR !== 'true',
-    watch: process.env.DISABLE_HMR === 'true' ? null : {},
-  },
+    server: {
+      hmr: process.env.DISABLE_HMR !== 'true',
+      watch: process.env.DISABLE_HMR === 'true' ? null : {},
+    },
+  }
 })
