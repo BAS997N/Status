f405673350fad3369c2fc23f79086ae41a9bf1c4
BAS997N <avielias0@gmail.com>
2026-06-16 19:41:58 +0300

    Update CommandDashboard.tsx

--- src/components/CommandDashboard.tsx
+++ src/components/CommandDashboard.tsx
@@ -300,13 +300,6 @@ const latestTodayReport = soldierReports.find(report =>
   };
 
   const statusList = getSoldiersLatestStatus();
-   {
-  selectedDate,
-  statusListCount: statusList.length,
-  reportedTodayListCount: statusList.filter(s => s.latestTodayReport).length,
-  reportsCount: reports.length,
-  firstReport: reports[0],
-});
 
   // Statistics Computations (Specifically for Today: June 10, 2026)
   const totalSoldiersCount = allSoldiers.filter(s => s.role !== "commander" && s.role !== "adjutant_officer" && !s.isDischarged).length;
